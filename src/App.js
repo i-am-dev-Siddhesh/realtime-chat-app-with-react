5e7e6683e56fb1b66eac8d8c48648616e422a635
i-am-dev-Siddhesh <shindesiddhesh.co.in@gmail.com>
2022-02-12 23:29:53 +0530

    feat:Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { ChatEngine } from "react-chat-engine";
+
+function App() {
+    return (
+        <ChatEngine
+            height="100vh"
+            width="100vw"
+            projectID=""
+            userName=""
+            userSecret=""
+        />
+    );
+}
+
+export default App;
